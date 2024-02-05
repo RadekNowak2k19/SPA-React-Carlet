8f97ce1841bbbae4b5fba5874df96c7f201478a1
Radosław Nowak <radeknowak2k19@gmail.com>
2024-02-05 19:57:34 +0100

    Improved header component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,7 +2,20 @@ import { X } from "@phosphor-icons/react";
 import { navigation } from "../../data";
 import styles from "./Navigation.module.scss";
 import { MobileNavElement } from "./MobileNavElement";
+import { useCallback, useEffect } from "react";
 export const Navigation = ({ setIsMobileNav }) => {
+	const onKeyDown = useCallback(
+		e => {
+			if (e.key === "Escape") setIsMobileNav(false);
+		},
+		[setIsMobileNav]
+	);
+	useEffect(() => {
+		window.addEventListener("keydown", e => onKeyDown(e));
+
+		return window.removeEventListener("keydown", onKeyDown);
+	}, [onKeyDown]);
+
 	return (
 		<div className={styles.opacity}>
 			<div></div>
